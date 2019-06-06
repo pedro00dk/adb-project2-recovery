7568bcd7ad9aa4395f916ef481dec00dc732e715
Pedro Henrique <pedro00dk@gmail.com>
2019-06-06 16:23:28 -0300

    Implement user input buttons

--- src/App.tsx
+++ src/App.tsx
@@ -43,25 +43,18 @@ export function App() {
             <nav className='navbar navbar-light bg-light'>
                 <span className='navbar-brand mb-0 h1'>FSDB</span>
             </nav>
-            <div className='d-flex flex-row flex-fill'>
-                <div className='d-flex flex-column align-items-center shadow w-25 m-2'>
+            <div className='d-flex flex-fill'>
+                <div className='d-flex flex-column shadow m-2' style={{ width: '33.33%' }}>
                     <h5 className='text-center shadow-sm p-2 mb-2 w-100'>PERSISTENT DATA</h5>
                     <FileSystemJournal fsPrefix='disk' />
                 </div>
-                <div className='d-flex flex-column align-items-center shadow w-25 m-2'>
+                <div className='d-flex flex-column shadow m-2' style={{ width: '33.33%' }}>
                     <h5 className='text-center shadow-sm p-2 mb-2 w-100'>VOLATILE DATA</h5>
                     <FileSystemJournal fsPrefix='mem' />
                 </div>
-                <div className='d-flex flex-column flex-fill align-items-center shadow m-2'>
-                    <button type='button' className='btn btn-secondary'>
-                        Secondary
-                    </button>
-                    <button type='button' className='btn btn-secondary'>
-                        Secondary
-                    </button>
-                    <button type='button' className='btn btn-secondary'>
-                        Secondary
-                    </button>
+                <div className='d-flex flex-column align-items-center shadow flex-fill m-2'>
+                    <RecoveryAlgorithms />
+                    <TransactionActions />
                 </div>
             </div>
         </div>
@@ -71,7 +64,7 @@ export function App() {
 function FileSystemJournal(props: { fsPrefix?: string }) {
     return (
         <>
-            <div className='d-flex flex-column shadow-sm mb-2 w-100 h-50'>
+            <div className='d-flex flex-column shadow-sm mb-2 w-100' style={{ height: '30%' }}>
                 <h6 className='text-center p-1 mb-1 w-100'>File System</h6>
                 <FileSystemTree fs={mock} fsPrefix={props.fsPrefix} />
             </div>
@@ -80,5 +73,64 @@ function FileSystemJournal(props: { fsPrefix?: string }) {
                 <JournalTable />
             </div>
         </>
+    )
+}
+
+function RecoveryAlgorithms(props: { chosenRA?: string; onChooseRA?: (algorithm: string) => void }) {
+    React.useEffect(() => {
+        if (props.chosenRA != undefined) props.onChooseRA('ru')
+    }, [])
+
+    return (
+        <div className='d-flex m-2 w-100'>
+            <button
+                type='button'
+                className={`btn ${
+                    !props.chosenRA || props.chosenRA === 'ru' ? 'btn-primary' : 'btn-outline-secondary'
+                } flex-fill m-2`}
+                style={{ width: '33%' }}
+                onClick={event => (!!props.onChooseRA ? props.onChooseRA('ru') : undefined)}
+            >
+                <span className='d-block'>imediate</span>
+                (REDO/UNDO)
+            </button>
+            <button
+                type='button'
+                className={`btn ${props.chosenRA === 'nru' ? 'btn-primary' : 'btn-outline-secondary'} flex-fill m-2`}
+                style={{ width: '33%' }}
+                onClick={event => (!!props.onChooseRA ? props.onChooseRA('nru') : undefined)}
+            >
+                <span className='d-block'>imediate</span>
+                (NO-REDO/UNDO)
+            </button>
+            <button
+                type='button'
+                className={`btn ${props.chosenRA === 'rnu' ? 'btn-primary' : 'btn-outline-secondary'} flex-fill m-2`}
+                style={{ width: '33%' }}
+                onClick={event => (!!props.onChooseRA ? props.onChooseRA('rnu') : undefined)}
+            >
+                <span className='d-block'>delayed</span>
+                (REDO/NO-UNDO)
+            </button>
+        </div>
+    )
+}
+
+function TransactionActions() {
+    return (
+        <div className='d-flex mx-2 mb-2 w-100'>
+            <button type='button' className='btn btn-outline-success flex-fill m-2 w-25'>
+                Start Transaction
+            </button>
+            <button type='button' className='btn btn-outline-primary flex-fill m-2 w-25'>
+                Commit Transaction
+            </button>
+            <button type='button' className='btn btn-outline-danger flex-fill m-2 w-25'>
+                Abort Transaction
+            </button>
+            <button type='button' className='btn btn-outline-warning flex-fill m-2 w-25'>
+                Manual GC
+            </button>
+        </div>
     )
 }
